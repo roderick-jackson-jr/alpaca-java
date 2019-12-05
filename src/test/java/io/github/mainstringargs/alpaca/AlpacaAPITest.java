869e01aa8bbf55779d518864ae0cb4ad9924b772
mainstringargs <mainstringargs@users.noreply.github.com>
2019-12-05 17:28:49 -0600

    #26 initial AlpacaAPITest skeleton

---
+++ src/test/java/io/github/mainstringargs/alpaca/AlpacaAPITest.java
@@ -0,0 +1,381 @@
+/**
+ * 
+ */
+package io.github.mainstringargs.alpaca;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AlpacaAPITest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {}
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {}
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {}
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {}
+
+    /**
+     * Test method for {@link io.github.mainstringargs.alpaca.AlpacaAPI#AlpacaAPI()}.
+     */
+    @Test
+    public final void testAlpacaAPI() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#AlpacaAPI(java.lang.String)}.
+     */
+    @Test
+    public final void testAlpacaAPIString() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#AlpacaAPI(java.lang.String, java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public final void testAlpacaAPIStringStringString() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#AlpacaAPI(java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public final void testAlpacaAPIStringStringStringString() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#AlpacaAPI(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public final void testAlpacaAPIStringStringStringStringString() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link io.github.mainstringargs.alpaca.AlpacaAPI#getAccount()}.
+     */
+    @Test
+    public final void testGetAccount() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#getAccountActivities(java.time.ZonedDateTime, java.time.ZonedDateTime, java.time.ZonedDateTime, io.github.mainstringargs.alpaca.enums.Direction, java.lang.Integer, java.lang.String, io.github.mainstringargs.alpaca.enums.ActivityType[])}.
+     */
+    @Test
+    public final void testGetAccountActivities() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link io.github.mainstringargs.alpaca.AlpacaAPI#getAccountConfiguration()}.
+     */
+    @Test
+    public final void testGetAccountConfiguration() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#setAccountConfiguration(io.github.mainstringargs.domain.alpaca.accountconfiguration.AccountConfiguration)}.
+     */
+    @Test
+    public final void testSetAccountConfiguration() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#getOrders(io.github.mainstringargs.alpaca.enums.OrderStatus, java.lang.Integer, java.time.ZonedDateTime, java.time.ZonedDateTime, io.github.mainstringargs.alpaca.enums.Direction)}.
+     */
+    @Test
+    public final void testGetOrders() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#requestNewOrder(java.lang.String, java.lang.Integer, io.github.mainstringargs.alpaca.enums.OrderSide, io.github.mainstringargs.alpaca.enums.OrderType, io.github.mainstringargs.alpaca.enums.OrderTimeInForce, java.lang.Double, java.lang.Double, java.lang.Boolean, java.lang.String)}.
+     */
+    @Test
+    public final void testRequestNewOrder() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#requestNewMarketOrder(java.lang.String, java.lang.Integer, io.github.mainstringargs.alpaca.enums.OrderSide, io.github.mainstringargs.alpaca.enums.OrderTimeInForce, java.lang.Boolean, java.lang.String)}.
+     */
+    @Test
+    public final void testRequestNewMarketOrder() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#requestNewLimitOrder(java.lang.String, java.lang.Integer, io.github.mainstringargs.alpaca.enums.OrderSide, io.github.mainstringargs.alpaca.enums.OrderTimeInForce, java.lang.Double, java.lang.Boolean, java.lang.String)}.
+     */
+    @Test
+    public final void testRequestNewLimitOrder() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#requestNewStopOrder(java.lang.String, java.lang.Integer, io.github.mainstringargs.alpaca.enums.OrderSide, io.github.mainstringargs.alpaca.enums.OrderTimeInForce, java.lang.Double, java.lang.Boolean, java.lang.String)}.
+     */
+    @Test
+    public final void testRequestNewStopOrder() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#requestNewStopLimitOrder(java.lang.String, java.lang.Integer, io.github.mainstringargs.alpaca.enums.OrderSide, io.github.mainstringargs.alpaca.enums.OrderTimeInForce, java.lang.Double, java.lang.Double, java.lang.Boolean, java.lang.String)}.
+     */
+    @Test
+    public final void testRequestNewStopLimitOrder() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link io.github.mainstringargs.alpaca.AlpacaAPI#getOrder(java.lang.String)}.
+     */
+    @Test
+    public final void testGetOrder() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#getOrderByClientID(java.lang.String)}.
+     */
+    @Test
+    public final void testGetOrderByClientID() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#replaceOrder(java.lang.String, java.lang.Integer, io.github.mainstringargs.alpaca.enums.OrderTimeInForce, java.lang.Double, java.lang.Double, java.lang.String)}.
+     */
+    @Test
+    public final void testReplaceOrder() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link io.github.mainstringargs.alpaca.AlpacaAPI#cancelAllOrders()}.
+     */
+    @Test
+    public final void testCancelAllOrders() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#cancelOrder(java.lang.String)}.
+     */
+    @Test
+    public final void testCancelOrder() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link io.github.mainstringargs.alpaca.AlpacaAPI#getOpenPositions()}.
+     */
+    @Test
+    public final void testGetOpenPositions() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#getOpenPositionBySymbol(java.lang.String)}.
+     */
+    @Test
+    public final void testGetOpenPositionBySymbol() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#getAssets(io.github.mainstringargs.alpaca.enums.AssetStatus, java.lang.String)}.
+     */
+    @Test
+    public final void testGetAssets() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#getAssetBySymbol(java.lang.String)}.
+     */
+    @Test
+    public final void testGetAssetBySymbol() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link io.github.mainstringargs.alpaca.AlpacaAPI#getWatchlists()}.
+     */
+    @Test
+    public final void testGetWatchlists() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#createWatchlist(java.lang.String, java.lang.String[])}.
+     */
+    @Test
+    public final void testCreateWatchlist() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#getWatchlist(java.lang.String)}.
+     */
+    @Test
+    public final void testGetWatchlist() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#updateWatchlist(java.lang.String, java.lang.String, java.lang.String[])}.
+     */
+    @Test
+    public final void testUpdateWatchlist() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#addWatchlistAssets(java.lang.String, java.lang.String[])}.
+     */
+    @Test
+    public final void testAddWatchlistAssets() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#deleteWatchlist(java.lang.String)}.
+     */
+    @Test
+    public final void testDeleteWatchlist() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#removeSymbolFromWatchlist(java.lang.String, java.lang.String)}.
+     */
+    @Test
+    public final void testRemoveSymbolFromWatchlist() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link io.github.mainstringargs.alpaca.AlpacaAPI#getCalendar()}.
+     */
+    @Test
+    public final void testGetCalendar() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#getCalendar(java.time.LocalDate, java.time.LocalDate)}.
+     */
+    @Test
+    public final void testGetCalendarLocalDateLocalDate() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link io.github.mainstringargs.alpaca.AlpacaAPI#getClock()}.
+     */
+    @Test
+    public final void testGetClock() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#getBars(io.github.mainstringargs.alpaca.enums.BarsTimeFrame, java.lang.String, java.lang.Integer, java.time.ZonedDateTime, java.time.ZonedDateTime, java.time.ZonedDateTime, java.time.ZonedDateTime)}.
+     */
+    @Test
+    public final void testGetBarsBarsTimeFrameStringIntegerZonedDateTimeZonedDateTimeZonedDateTimeZonedDateTime() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#getBars(io.github.mainstringargs.alpaca.enums.BarsTimeFrame, java.lang.String[], java.lang.Integer, java.time.ZonedDateTime, java.time.ZonedDateTime, java.time.ZonedDateTime, java.time.ZonedDateTime)}.
+     */
+    @Test
+    public final void testGetBarsBarsTimeFrameStringArrayIntegerZonedDateTimeZonedDateTimeZonedDateTimeZonedDateTime() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#addAlpacaStreamListener(io.github.mainstringargs.alpaca.websocket.listener.AlpacaStreamListener)}.
+     */
+    @Test
+    public final void testAddAlpacaStreamListener() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link io.github.mainstringargs.alpaca.AlpacaAPI#removeAlpacaStreamListener(io.github.mainstringargs.alpaca.websocket.listener.AlpacaStreamListener)}.
+     */
+    @Test
+    public final void testRemoveAlpacaStreamListener() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link io.github.mainstringargs.alpaca.AlpacaAPI#toString()}.
+     */
+    @Test
+    public final void testToString() {
+        fail("Not yet implemented");
+    }
+
+}
